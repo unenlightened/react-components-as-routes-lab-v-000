8b64a7b2225938a1daf2c5679af17a1534706afb
Chely Ho <adept_inept@yahoo.com>
2018-09-28 20:58:44 +0000

    automatically backed up by learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actor = ({ name, movies }) => {
-  <div className='actor'>
-    <h2>{name}</h2>
-    Movies:
-    <ul>
-      {movies.map(movie => <li>{movie}</li>)}
-    <ul>
-  </div>
+  return (
+    <div className='actor'>
+      <h2>{name}</h2>
+      Movies:
+      <ul>
+        {movies.map(movie => <li>{movie}</li>)}
+      <ul>
+    </div>
+  )
 }
 
 const Actors = (props) => {
