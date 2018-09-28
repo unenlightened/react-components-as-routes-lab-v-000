20e0b44b7223f142a66f4aa9376f89bda9aaa39c
Chely Ho <adept_inept@yahoo.com>
2018-09-28 20:54:50 +0000

    automatically backed up by learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { actors } from '../data';
 
-const Actors = () => {
+const Actor = () =>
+
+const Actors = (props) => {
   return (
     <div>
       <h1>Actors Page</h1>
-
+      {this.props.actors.map(actor => Actor)}
     </div>
   );
 };
